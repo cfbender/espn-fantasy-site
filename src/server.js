7846ae7590c4099d256452617c260caac8f07d76
cfbender <codyfbender@gmail.com>
2019-09-30 12:02:46 -0600

    Move ESPN info to .env

--- src/server.js
+++ src/server.js
@@ -2,16 +2,16 @@ const express = require("express");
 const path = require("path");
 const moment = require("moment");
 const sortBy = require("lodash.sortby");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-const { Client } = require("espn-fantasy-football-api/node-dev");
 
-const myClient = new Client({ leagueId: 38034 });
+const { Client } = require("espn-fantasy-football-api/node-dev");
+const myClient = new Client({ leagueId: process.env.LEAGUEID });
 myClient.setCookies({
-  espnS2:
-    "AECn4e3%2BhT7TyVSPL9KxNDfxetQLcKyQuor0Sl30uzkggZtrSxgCfoLvI98Lm5ojjAsihAekSKRVl4nrvxis21kzLZ%2BVEwfCtksxBgNHLqwq8gOErpkR4fC4fOmth6bDFsNRozPtWNAvXD%2FaiGhyX08Goxwf7%2FZCaCaF4tsBGvMgbFeYZh%2FQJTW57E2IyDQwsjb0Jku2njUhj9ga5ki1MB488%2Fka4VZ03dnkDTvvMgiubYjbJWrThWjZlVnERKHollGUOTP9hgPfBds4KJcPyDPN",
-  SWID: "{0B64B93E-FFBF-488E-8630-EF94F6EBD636}"
+  espnS2: process.env.ESPNS2,
+  SWID: process.env.SWID
 });
 
 /*
